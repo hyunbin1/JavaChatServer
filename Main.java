daba8d446c132a0b705e493c60d84784b85328c6
hyunbin1 <kimgusqls1@gmail.com>
2021-01-12 15:39:34 +0900

    waiting Clients connection

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 package application;
 	
+import java.net.ServerSocket;
 import java.util.Vector;
 import java.util.concurrent.ExecutorService;
 
@@ -12,10 +13,15 @@ import javafx.scene.layout.BorderPane;
 public class Main extends Application {
 	// 사용자들이 접속했을때 효과적으로 thread를 관리
 	public static ExecutorService threadPool;
+	//쉽게 사용할 수 있는 배열
 	public static Vector<Client> clients = new Vector<Client>();
 	
+	ServerSocket serverSocket;
 	
-	
+	// 서버를 구동시켜서 클라이언트의 연결을 기다리는 메소드 입니다. 
+	public void startServer(String IP, int port) {
+		
+	}
 	
 	@Override
 	public void start(Stage primaryStage) {
